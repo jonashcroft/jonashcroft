1490bcd89d156ba911664e09e6f7ce729f544681
Jon Ashcroft <hello@jonashcroft.co.uk>
2018-05-10 07:20:27 +0100

    Fix: category.php and tag.php were missing, meaning the archives didn't work.

---
+++ category.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<main>
+<?php
+    global $post; // required
+    $args = [
+            'post_type'    => 'post',
+            'numberposts'  => -1,
+            'cat'          => get_queried_object()->term_id
+            ];
+    $allPosts = get_posts($args);
+
+    foreach($allPosts as $post) { setup_postdata($post); ?>
+        <article class="post-item">
+            <h2><a href="<?php echo get_permalink($post->ID); ?>" title="Read '<?php echo $post->post_title; ?>'"><?php echo $post->post_title; ?></a></h2>
+            <span class="date"><?php echo date('jS M Y', strtotime($post->post_date) ); ?></span>
+            <p><?php echo wp_trim_words($post->post_content, '50'); ?></p>
+        </article>
+
+    <?php } ?>
+</main>
+
+<?php get_footer(); ?>
