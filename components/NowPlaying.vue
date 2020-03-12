1ccea9cbaf0660d0090e197d4f8004e58ef14ccd
Jon Ashcroft <hello@jonashcroft.co.uk>
2020-03-12 22:28:57 +0000

    BUILD: Add last.fm 'Now playing' component

---
+++ components/NowPlaying.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1 class="tv-screen__white-text">{{ trackArtist }} - {{ trackName }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      results: [],
+      trackName: '',
+      trackArtist: '',
+      trackCover: '',
+      test: process.env.LASTFM_API_KEY
+    }
+  },
+
+  mounted() {
+    axios
+      .get(
+        `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=wreckages&limit=2&api_key=fa2c19d07e45047e18abfd9d3b21632c&format=json`
+      )
+      .then((response) => {
+        this.results = response.data.recenttracks
+        this.trackName = response.data.recenttracks.track[0].name
+        this.trackArtist = response.data.recenttracks.track[0].artist['#text']
+        this.trackCover = response.data.recenttracks.track[0].image[2]['#text']
+        this.displayNowPlaying()
+      })
+  },
+  methods: {
+    displayNowPlaying() {
+      return this.trackName
+    }
+  }
+}
+</script>
--- components/Vcr.vue
+++ components/Vcr.vue
@@ -3,7 +3,19 @@
     <div class="tv-screen__init">AV-1</div>
 
     <h1 class="tv-screen__white-text">hello, vcr</h1>
+
+    <NowPlaying />
   </div>
 </template>
+
+<script>
+import NowPlaying from '~/components/NowPlaying.vue'
+
+export default {
+  components: {
+    NowPlaying
+  }
+}
+</script>
 
 <style src="~/assets/styles/components/vcr.scss" lang="scss"></style>
