a88556bf6b942ed640f05a7bfc64e8bca39d8e46
Jon Ashcroft <hello@jonashcroft.co.uk>
2020-03-12 21:30:25 +0000

    BUILD: Add basic turn-on, AV-1 and intro animations

--- components/Vcr.vue
+++ components/Vcr.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="tv-screen">
-    <h1>hello, vcr</h1>
+    <div class="tv-screen__init">AV-1</div>
+
+    <h1 class="tv-screen__white-text">hello, vcr</h1>
   </div>
 </template>
 
