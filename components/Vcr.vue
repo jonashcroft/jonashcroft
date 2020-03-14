27325821c713b931eee608795ddbc40038a98080
Jon Ashcroft <hello@jonashcroft.co.uk>
2020-03-14 21:39:24 +0000

    BUILD: Add various mixin for link and focus hover, basic tabs

--- components/Vcr.vue
+++ components/Vcr.vue
@@ -6,7 +6,59 @@
   >
     <div class="container">
       <div class="tv-screen__init">AV-1</div>
-      <h1 class="tv-screen__white-text tv-screen__intro-heading">hello, vcr</h1>
+      <h1 class="tv-screen__white-text tv-screen__intro-heading" tabindex="0">
+        Jon Ashcroft
+      </h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste
+        doloremque repudiandae nemo architecto quidem nulla temporibus ex,
+        eveniet, nobis quis earum rerum accusamus fugit ut ad nesciunt, soluta
+        natus hic ullam odio vero.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste
+        doloremque repudiandae nemo architecto quidem nulla temporibus ex,
+        eveniet, nobis quis earum rerum accusamus fugit ut ad nesciunt, soluta
+        natus hic ullam odio vero.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste
+        doloremque repudiandae nemo architecto quidem nulla temporibus ex,
+        eveniet, nobis quis earum rerum accusamus fugit ut ad nesciunt, soluta
+        natus hic ullam odio vero.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste
+        doloremque repudiandae nemo architecto quidem nulla temporibus ex,
+        eveniet, nobis quis earum rerum accusamus fugit ut ad nesciunt, soluta
+        natus hic ullam odio vero.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste
+        doloremque repudiandae nemo architecto quidem nulla temporibus ex,
+        eveniet, nobis quis earum rerum accusamus fugit ut ad nesciunt, soluta
+        natus hic ullam odio vero.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste
+        doloremque repudiandae nemo architecto quidem nulla temporibus ex,
+        eveniet, nobis quis earum rerum accusamus fugit ut ad nesciunt, soluta
+        natus hic ullam odio vero.
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro iste
+        doloremque repudiandae nemo architecto quidem nulla temporibus ex,
+        eveniet, nobis quis earum rerum accusamus fugit ut ad nesciunt, soluta
+        natus hic ullam odio vero.
+      </p>
+
       <NowPlaying @updateColours="doColours" />
     </div>
   </div>
